e45b8b0fd4c7cb7245ef036b7f49447a6184ebad
Yuu Mizusaki <2015084252@ust-ics.mygbiz.com>
2018-11-26 21:24:53 +0800

    .

--- app/src/main/java/com/example/jky/dietary/MainActivity.java
+++ app/src/main/java/com/example/jky/dietary/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* setContentView(R.layout.register_page);
-        EditText email1 = (EditText) findViewById(R.id.email);
-        EditText userN1 = (EditText) findViewById(R.id.userN);
-        EditText passW1 = (EditText) findViewById(R.id.passW); */
-
     }
+       public void startLog(View v){
+           startActivity(new Intent(MainActivity.this, day_one_breakfast.class));
+       }
     public void startReg(View v){
         startActivity(new Intent(MainActivity.this, register.class));
     }
-    public void startLog(View v){
-        startActivity(new Intent( MainActivity.this, day_one_breakfast.class));
-    }
+
+
+
     }
 
